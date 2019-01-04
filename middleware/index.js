03d54ef56760b322a0c3a8dafeb3af7fedb98bd1
yuang.zya <yuang.zya@alibaba-inc.com>
2019-01-04 10:18:15 +0800

    feat: 自定义参数改为Object.assign

--- middleware/index.js
+++ middleware/index.js
@@ -3,16 +3,16 @@ const hotMiddleware = require('./hotMiddleware')
 const compose = require('koa-compose')
 
 module.exports = (compile, devConf, hotConf) => {
-    return compose([
-        devMiddleware(compile, devConf||{
-            noInfo: false,
-            hot: true,
-            serverSideRender: true
-        }),
-        hotMiddleware(compile, hotConf||{
-            log: console.log,
-            path: "/__webpack_hmr",
-            heartbeat: 2000
-        })
-    ])
+  return compose([
+    devMiddleware(compile, Object.assign({
+      noInfo: false,
+      hot: true,
+      serverSideRender: true
+    }, devConf)),
+    hotMiddleware(compile, Object.assign({
+      log: console.log,
+      path: '/__webpack_hmr',
+      heartbeat: 2000
+    }, hotConf))
+  ])
 }
